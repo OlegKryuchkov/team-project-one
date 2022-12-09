01a8fe7aac8df3eb62520b66f47e90abfec175ab
BoikoIgor <111757412+BoikoIgor@users.noreply.github.com>
2022-12-09 18:08:43 +0000

    Deploying to gh-pages from @ BoikoIgor/team-project-one@ff8f158ee13d738961c2fafc05c64f82503c2e6e 🚀

---
+++ index.1faabbce.js
@@ -0,0 +1,2 @@
+$(".fade").slick({dots:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear",autoplay:!0}),$(".fade2").slick({dots:!0,infinite:!0,speed:500,slidesToShow:2,fade:!0,cssEase:"linear",autoplay:!0});
+//# sourceMappingURL=index.1faabbce.js.map
--- index.bb06daa0.js
+++
@@ -1,2 +0,0 @@
-$(".fade").slick({dots:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear",autoplay:!0});
-//# sourceMappingURL=index.bb06daa0.js.map
