ff8f158ee13d738961c2fafc05c64f82503c2e6e
Igor Boiko <igorboiko@gmail.com>
2022-12-09 20:08:19 +0200

    add js slider for 2 images

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,7 +1,18 @@
+// Slider for one BG images gallery
 $('.fade').slick({
   dots: true,
   infinite: true,
   speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+});
+// Slider for two images gallery
+$('.fade2').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 2,
   fade: true,
   cssEase: 'linear',
   autoplay: true,
