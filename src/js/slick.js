f593a799bd3b49740b9f29ed3dee88821c6eee8a
INovak90 <112341296+INovak90@users.noreply.github.com>
2022-12-13 15:37:15 +0200

    Переделал всё

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,11 +1,13 @@
 // Slider for one BG images gallery
 $('.fade').slick({
-  dots: true,
+  dots: false,
   infinite: true,
   speed: 500,
   fade: true,
-  cssEase: 'linear',
-  autoplay: true,
+  cssEase: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  autoplay: false,
+  arrows: true,
+  adaptiveHeight: true,
 });
 // Slider for two images gallery
 $('.fade2').slick({
