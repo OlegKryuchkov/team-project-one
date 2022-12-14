cd0421c01ffbbec47ececc574ca69182d3401799
Igor Boiko <igorboiko@gmail.com>
2022-12-14 09:27:56 +0200

    add arrows for hovers

--- src/js/slick.js
+++ src/js/slick.js
@@ -5,17 +5,20 @@ $('.fade').slick({
   speed: 500,
   fade: true,
   cssEase: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
-  autoplay: false,
+  autoplay: true,
   arrows: true,
   adaptiveHeight: true,
 });
+
 // Slider for two images gallery
 $('.fade2').slick({
-  dots: false,
+  // dots: false,
+  adaptiveHeight: true,
   infinite: true,
   speed: 500,
   slidesToShow: 2,
   fade: true,
   cssEase: 'linear',
   autoplay: true,
+  adaptiveHeight: true,
 });
